fec15daa815ce13209695800ad5a344612dd7842
CristinaSeician <cristinaseician@yahoo.com>
2022-11-30 18:46:23 +0200

    afiseaza_ceva

---
+++ src/App.vue
@@ -0,0 +1,105 @@
+<script setup>
+import afiseaza from "./components/afiseazaTari.vue";
+</script>
+
+<template>
+  <header>
+   
+  </header>
+
+  <main>
+    <afiseaza/>
+  </main>
+</template>
+
+<style scoped>
+
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
+
+
+<!-- <script setup>
+import Api from "./components/icons/Api.vue";
+import Home from "./components/icons/Home.vue";
+import Admin from "./components/icons/Admin.vue";
+import AddExericesVue from "./components/icons/AddExerices.vue";
+import ExercicesPageVue from "./components/icons/ExercicesPage.vue";
+import { useTheme } from "vuetify";
+</script>
+<script>
+const routes = {
+
+
+  "/": Home,
+  "/ExercicesPageVue":ExercicesPageVue,
+  "/Api":Api,
+  "/Admin":Admin,
+  "/AddExericesVue":AddExericesVue
+
+};
+export default {
+  setup() {
+    const theme = useTheme();
+
+  },
+  data() {
+    return {
+      currentPath: window.location.hash,
+    };
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1) || "/"];
+    },
+  },
+  mounted() {
+    window.addEventListener("hashchange", () => {
+      this.currentPath = window.location.hash;
+    });
+  },
+  methods: {
+
+  },
+};
+</script>
+<template>
+
+  <main>
+    <v-app>
+      <v-app-bar title="Code Remote">
+        <v-btn href="#/">Home</v-btn>
+        <v-btn href="#/ExercicesPageVue">Challange</v-btn>
+        <v-btn href="#/Api">Play</v-btn>
+      </v-app-bar>
+      <v-main>
+        <component :is="currentView" />
+      </v-main>
+<v-footer>
+  <v-btn href="#/Admin">Admin</v-btn>
+</v-footer>
+
+    </v-app>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
+-->
